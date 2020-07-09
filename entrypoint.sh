47353a896e3b52975181c2fc849b70f2ad6a9d3b
Benoit Verret <benoit.verret.1988@gmail.com>
2020-07-09 13:36:58 -0400

    Setting test workflow

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,5 @@
 echo "Enabling experimental feature"
 mkdir -p ~/.docker && echo '{"experimental": "enabled"}' > ~/.docker/config.json
 echo "Login to docker registry $REGISTRY with user $USERNAME"
-echo "$PASSWORD" | docker login --username $USERNAME --password-stdin $REGISTRY
+echo "PASSWORD" | docker login --username "USERNAME" --password-stdin $REGISTRY
 echo "::set-output name=image_exist::$(docker manifest inspect $IMAGE > /dev/null && echo 0 || echo 1)"
