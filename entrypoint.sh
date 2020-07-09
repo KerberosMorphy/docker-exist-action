b6b7e6387cc46d5a92e4c1a811aa4389c71c4934
Benoit Verret <benoit.verret.1988@gmail.com>
2020-07-09 13:01:34 -0400

    Setting test workflow

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -l
 
 echo "Login to docker registry $REGISTRY with user $USERNAME"
-echo docker login $REGISTRY --username $USERNAME --password $PASSWORD
+docker login $REGISTRY --username $USERNAME --password $PASSWORD
 echo "::set-output name=image_exist::$(docker manifest inspect $IMAGE > /dev/null && echo 0 || echo 1)"
